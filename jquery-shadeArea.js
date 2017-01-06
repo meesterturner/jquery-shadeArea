05df938ab3bd8d35ada89b915c48df8049e88b5b
Paul Turner <paul.turner611@gmail.com>
2017-01-06 22:59:47 +0000

    Fixed numerous bugs, and added an "id" parameter

--- jquery-shadeArea.js
+++ jquery-shadeArea.js
@@ -5,31 +5,68 @@
 			// Set some defaults up...
 			var settings = {
 				opacity: 0.7,
-				color: "black"
+				color: "black",
+				id: null
 			}
 			
 			// Merge user specified options into the settings
 			$.extend(settings, options);
 		
 			// Grab the element we want to shade the background for
+			// and some other details related to it...
 			var thingToShade = $(this);
+			var origZ = thingToShade.zIndex;
+			if(origZ == null) {
+				origZ = 1;
+			}
 			
+			var origTop  = thingToShade.offset().top;
+			var origLeft = thingToShade.offset().left;
+			
+			// Generate a new ID, hopefully unique
+			var newId = settings.id;
+			var idPrefix = "shadeArea_";
+			
+			if(newId == null  || newId == "") {
+				if(thingToShade.id == null || thingToShade.id == "") {
+					newId = idPrefix + origTop.toString() + origLeft.toString() + origZ.toString();
+				} else {
+					newId = idPrefix + thingToShade.id;
+				} 
+			}
+
 			// Create a new div and apply it before the original content
-			var shader = $("<div></div>").css({
+			var newDiv = "<div id=\"" + newId + "\"></div>";
+			
+			var shader = $(newDiv).css({
+				"position": "absolute",
 				"width": thingToShade.width(),
 				"height": thingToShade.height(),
+				"borderTop": thingToShade.css("borderTopWidth"),
+				"borderLeftWidth": thingToShade.css("borderLeftWidth"),
+				"borderBottom": thingToShade.css("borderBottomWidth"),
+				"borderRightWidth": thingToShade.css("borderRightWidth"),
+				"borderStyle": "solid",
+				"top": origTop,
+				"left": origLeft,
+				"zIndex": origZ,
 				"backgroundColor": settings.color,
 				"opacity": settings.opacity
 			});
 			
-			$(this).before(shader);
+			if(thingToShade.is(":visible") === false) {
+				shader.css("display", "none");
+			}
 			
-			// Move original content up
-			var newTop = 0 - thingToShade.height();
+			$("body").append(shader);
 			
-			$(this).css({
-				"position": "relative",
-				"top": newTop
+			thingToShade.css({
+				"position": "absolute",
+				"width": thingToShade.width(),
+				"height": thingToShade.height(),
+				"top": origTop,
+				"left": origLeft,
+				"zIndex": origZ + 1
 			});
 		}); 
 	};
